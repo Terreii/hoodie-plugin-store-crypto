3939012a91a7f011b480cbecc130815df6550131
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-02-04 18:04:48 +0100

    test(store): add more tests for add

--- tests/integration/add.js
+++ tests/integration/add.js
@@ -47,3 +47,132 @@ test('adds object to Store', function (t) {
     t.ok(res.nonce, 'has nonce')
   })
 })
+
+test('fails for invalid object', function (t) {
+  t.plan(2)
+
+  var name = uniqueName()
+  var store = new Store(name, {
+    PouchDB: PouchDB,
+    remote: 'remote-' + name
+  })
+  var hoodie = {
+    store: store
+  }
+  cryptoStore(hoodie)
+
+  hoodie.cryptoStore.setPassword('test')
+
+  .then(function () {
+    return hoodie.cryptoStore.add()
+  })
+
+  .catch(function (err) {
+    t.ok(err instanceof Error, 'rejects with an error')
+    t.is(err.status, 400, 'rejects with error 400')
+  })
+})
+
+test('fails for existing object', function (t) {
+  t.plan(2)
+
+  var name = uniqueName()
+  var store = new Store(name, {
+    PouchDB: PouchDB,
+    remote: 'remote-' + name
+  })
+  var hoodie = {
+    store: store
+  }
+  cryptoStore(hoodie)
+
+  hoodie.cryptoStore.setPassword('test')
+
+  .then(function () {
+    return hoodie.cryptoStore.add({
+      _id: 'foo',
+      bar: 1
+    })
+  })
+
+  .then(function () {
+    return hoodie.cryptoStore.add({
+      _id: 'foo',
+      bar: 2
+    })
+  })
+
+  .catch(function (err) {
+    t.ok(err instanceof Error, 'rejects with an error')
+    t.is(err.status, 409, 'rejects with error 409')
+  })
+})
+
+test('adds multiple objects to db', function (t) {
+  t.plan(19)
+
+  var name = uniqueName()
+  var store = new Store(name, {
+    PouchDB: PouchDB,
+    remote: 'remote-' + name
+  })
+  var hoodie = {
+    store: store
+  }
+  cryptoStore(hoodie)
+
+  hoodie.cryptoStore.setPassword('test')
+
+  .then(function () {
+    return hoodie.cryptoStore.add({
+      _id: 'foo',
+      bar: 1
+    })
+  })
+
+  .then(function () {
+    return hoodie.cryptoStore.add([{
+      foo: 'bar'
+    }, {
+      foo: 'baz'
+    }, {
+      _id: 'foo',
+      foo: 'baz'
+    }])
+  })
+
+  .then(function (objects) {
+    t.is(objects[0].foo, 'bar', 'resolves first value')
+    t.ok(objects[0]._id, 'resolves first id')
+    t.ok(objects[0]._rev, 'resolves first _rev')
+
+    t.is(objects[1].foo, 'baz', 'resolves second value')
+    t.ok(objects[1]._id, 'resolves second id')
+    t.ok(objects[1]._rev, 'resolves second _rev')
+
+    t.ok(objects[2] instanceof Error, 'resolves third with error')
+
+    return store.get([
+      objects[0]._id,
+      objects[1]._id,
+      'foo'
+    ])
+  })
+
+  .then(function (res) {
+    t.is(res[0].foo, undefined, 'first stored doc has no foo')
+    t.ok(res[0].data, 'first has encrypted data')
+    t.ok(res[0].tag, 'first has tag')
+    t.ok(res[0].nonce, 'first has nonce')
+
+    t.is(res[1].foo, undefined, 'second stored doc has no foo')
+    t.ok(res[1].data, 'second has encrypted data')
+    t.ok(res[1].tag, 'second has tag')
+    t.ok(res[1].nonce, 'second has nonce')
+
+    t.is(res[2].foo, undefined, 'third stored doc has no foo')
+    t.ok(res[2].data, 'third has encrypted data')
+    t.ok(res[2].tag, 'third has tag')
+    t.ok(res[2].nonce, 'third has nonce')
+  })
+})
