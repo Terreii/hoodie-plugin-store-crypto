a377d8d0b3c38f3aedb48293fc7d74d359f2c252
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-05-18 16:54:27 +0200

    test(update & remove): give more time for travis

--- tests/integration/remove-all.js
+++ tests/integration/remove-all.js
@@ -10,7 +10,7 @@ function checkTime (objectTime) {
   var timeObj = new Date(objectTime)
   var isoString = timeObj.toISOString()
   var time = timeObj.getTime()
-  return time <= now && time > (now - 5) && objectTime === isoString
+  return time <= now && time > (now - 10) && objectTime === isoString
 }
 
 test('cryptoStore.removeAll()', function (t) {
