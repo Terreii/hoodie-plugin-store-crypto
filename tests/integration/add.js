3655f830d3516177d18cd2824e90f536286c90de
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-05-21 17:38:04 +0200

    test: add code heritage info

--- tests/integration/add.js
+++ tests/integration/add.js
@@ -1,5 +1,11 @@
 'use strict'
 
+/*
+ * Testing the cryptoStore.add method.
+ *
+ * Most tests are copied and adjusted from https://github.com/hoodiehq/hoodie-store-client
+ */
+
 var test = require('tape')
 
 var createCryptoStore = require('../utils/createCryptoStore')
--- tests/integration/update-all.js
+++ tests/integration/update-all.js
@@ -1,5 +1,11 @@
 'use strict'
 
+/*
+ * Testing the cryptoStore.updateAll method.
+ *
+ * Most tests are copied and adjusted from https://github.com/hoodiehq/hoodie-store-client
+ */
+
 var test = require('tape')
 var Promise = require('lie')
 
