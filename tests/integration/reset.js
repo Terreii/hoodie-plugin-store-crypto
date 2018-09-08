777f8facfc897d8754a553748fa5679661168655
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-09-08 13:56:41 +0200

    test(reset): add tests for methods that use db.allDocs

---
+++ tests/integration/reset.js
@@ -0,0 +1,89 @@
+'use strict'
+
+var test = require('tape')
+
+var createCryptoStore = require('../utils/createCryptoStore')
+
+test('cryptoStore.changePassword() works after a reset of the hoodie store', function (t) {
+  t.plan(1)
+
+  var hoodie = createCryptoStore()
+
+  hoodie.store.reset()
+
+    .then(function () {
+      return hoodie.cryptoStore.setPassword('test')
+    })
+
+    .then(function () {
+      return hoodie.cryptoStore.changePassword('test', 'otherPassword')
+    })
+
+    .then(function () {
+      t.pass('ok')
+    })
+
+    .catch(t.error)
+})
+
+test('cryptoStore.findAll() works after a reset of the hoodie store', function (t) {
+  t.plan(1)
+
+  var hoodie = createCryptoStore()
+
+  hoodie.store.reset()
+
+    .then(function () {
+      return hoodie.cryptoStore.findAll()
+    })
+
+    .then(function () {
+      t.pass('ok')
+    })
+
+    .catch(t.error)
+})
+
+test('cryptoStore.updateAll() works after a reset of the hoodie store', function (t) {
+  t.plan(1)
+
+  var hoodie = createCryptoStore()
+
+  hoodie.store.reset()
+
+    .then(function () {
+      return hoodie.cryptoStore.add({value: 'a'}, {value: 'b'})
+    })
+
+    .then(function () {
+      return hoodie.cryptoStore.updateAll({other: 'c'})
+    })
+
+    .then(function () {
+      t.pass('ok')
+    })
+
+    .catch(t.error)
+})
+
+test('cryptoStore.removeAll() works after a reset of the hoodie store', function (t) {
+  t.plan(1)
+
+  var hoodie = createCryptoStore()
+
+  hoodie.store.reset()
+
+    .then(function () {
+      return hoodie.cryptoStore.add({value: 'a'}, {value: 'b'})
+    })
+
+    .then(function () {
+      return hoodie.cryptoStore.removeAll()
+    })
+
+    .then(function () {
+      t.pass('ok')
+    })
+
+    .catch(t.error)
+})
