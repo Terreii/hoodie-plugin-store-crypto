cdec9571919bd71b5d6a25111d356a32fb878196
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-09-15 23:57:38 +0200

    fix(removeAll): works after reset

--- tests/integration/reset.js
+++ tests/integration/reset.js
@@ -27,25 +27,35 @@ test('cryptoStore.changePassword() works after a reset of the hoodie store', fun
 })
 
 test('cryptoStore.findAll() works after a reset of the hoodie store', function (t) {
-  t.plan(1)
+  t.plan(3)
 
   var hoodie = createCryptoStore()
 
   hoodie.store.reset()
 
     .then(function () {
-      return hoodie.cryptoStore.findAll()
+      return hoodie.cryptoStore.setPassword('test')
     })
 
     .then(function () {
-      t.pass('ok')
+      return hoodie.cryptoStore.add([{value: 'a'}, {value: 'b'}])
+    })
+
+    .then(function () {
+      return hoodie.cryptoStore.findAll()
+    })
+
+    .then(function (docs) {
+      t.equal(docs.length, 2, 'did find 2')
+      t.equal(docs[0].value, 'a', 'did decrypt')
+      t.equal(docs[1].value, 'b', 'did decrypt')
     })
 
     .catch(t.error)
 })
 
 test('cryptoStore.updateAll() works after a reset of the hoodie store', function (t) {
-  t.plan(1)
+  t.plan(3)
 
   var hoodie = createCryptoStore()
 
@@ -56,22 +66,24 @@ test('cryptoStore.updateAll() works after a reset of the hoodie store', function
     })
 
     .then(function () {
-      return hoodie.cryptoStore.add({value: 'a'}, {value: 'b'})
+      return hoodie.cryptoStore.add([{value: 'a'}, {value: 'b'}])
     })
 
     .then(function () {
       return hoodie.cryptoStore.updateAll({other: 'c'})
     })
 
-    .then(function () {
-      t.pass('ok')
+    .then(function (docs) {
+      t.equal(docs.length, 2, 'did update 2')
+      t.equal(docs[0].other, 'c', 'was updated')
+      t.equal(docs[1].other, 'c', 'was updated')
     })
 
     .catch(t.error)
 })
 
 test('cryptoStore.removeAll() works after a reset of the hoodie store', function (t) {
-  t.plan(1)
+  t.plan(3)
 
   var hoodie = createCryptoStore()
 
@@ -82,15 +94,17 @@ test('cryptoStore.removeAll() works after a reset of the hoodie store', function
     })
 
     .then(function () {
-      return hoodie.cryptoStore.add({value: 'a'}, {value: 'b'})
+      return hoodie.cryptoStore.add([{value: 'a'}, {value: 'b'}])
     })
 
     .then(function () {
       return hoodie.cryptoStore.removeAll()
     })
 
-    .then(function () {
-      t.pass('ok')
+    .then(function (docs) {
+      t.equal(docs.length, 2, 'did delete 2')
+      t.equal(docs[0]._deleted, true, 'was deleted')
+      t.equal(docs[1]._deleted, true, 'was deleted')
     })
 
     .catch(t.error)
