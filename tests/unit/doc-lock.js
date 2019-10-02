ab9eec75955cee5e6cd8ba181256cd86dbeb7d82
Christopher Astfalk <christopher.astfalk@icloud.com>
2019-10-02 16:26:04 +0200

    refactor(doc-lock): remove throwing while locking multiple ids

--- tests/unit/doc-lock.js
+++ tests/unit/doc-lock.js
@@ -4,25 +4,32 @@ var test = require('tape')
 
 var docLock = require('../../lib/utils/doc-lock')
 
-test('docLock should return a unlock function', function (t) {
-  t.plan(1)
+test(
+  'docLock should return a object with an unlock function and lists of locked docs and failures',
+  function (t) {
+    t.plan(4)
 
-  var unlock = docLock(null, 'firstTest')
+    var locked = docLock(null, 'firstTest')
 
-  t.is(typeof unlock, 'function', 'returns a function')
+    t.is(typeof locked, 'object', 'returns an object')
+    t.is(typeof locked.unlock, 'function', 'with a function')
 
-  unlock()
+    t.ok(Array.isArray(locked.failed), 'has an Array of failed ids')
+    t.deepEqual(locked.failed, [null], 'failed Array contains one null')
 
-  t.end()
-})
+    locked.unlock()
 
-test('docLock should unlock if the returned function is called', function (t) {
+    t.end()
+  }
+)
+
+test('docLock should unlock if the unlock function is called', function (t) {
   try {
-    var unlock = docLock(null, 'test')
+    var locked = docLock(null, 'test')
 
-    unlock()
+    locked.unlock()
 
-    docLock(null, 'test')()
+    docLock(null, 'test').unlock()
 
     t.pass('did unlock')
     t.end()
@@ -31,43 +38,67 @@ test('docLock should unlock if the returned function is called', function (t) {
   }
 })
 
-test('docLock should throw an conflict error if a document was already locked', function (t) {
-  var unlock = docLock(null, 'test')
+test('docLock should work with a string and an object', function (t) {
+  t.plan(1)
+
+  var locked1 = docLock(null, 'test')
 
   try {
-    docLock(null, 'test')()
+    var locked2 = docLock(null, { _id: 'test' })
+    locked2.unlock()
+    t.fail('should have thrown')
+  } catch (err) {
+    t.is(err.status, 409, 'did error with conflict error')
+  } finally {
+    locked1.unlock()
+  }
+})
+
+test('docLock should throw an conflict error if a document was already locked', function (t) {
+  var locked = docLock(null, 'test')
+
+  try {
+    docLock(null, 'test').unlock()
     t.fail("shouldn't fail with same prefix")
   } catch (err) {
     t.ok('does throw')
   } finally {
-    unlock()
+    locked.unlock()
   }
 
   t.end()
 })
 
 test('docLock should handle multiple docs', function (t) {
-  var unlock = docLock(null, ['test', 'other'])
+  var locked = docLock(null, ['test', { _id: 'other' }])
+
+  t.deepEqual(locked.failed, [null, null], 'has an array with errors or null by id index')
 
   try {
-    docLock(null, 'test')()
-    t.fail("shouldn't fail with same prefix")
+    docLock(null, 'test').unlock()
+    t.fail('should throw with same prefix')
   } catch (err) {
     t.ok('does throw')
   }
 
   try {
-    docLock(null, 'other')()
-    t.fail("shouldn't fail with same prefix")
+    docLock(null, 'other').unlock()
+    t.fail('should throw with same prefix')
   } catch (err) {
     t.ok('does throw')
   }
 
-  unlock()
+  var otherLocked = docLock(null, ['test', 'notLocked'])
+  t.ok(otherLocked.failed[0] instanceof Error, 'already locked doc has an error')
+  t.is(otherLocked.failed[1], null, 'new lock has null in failed array')
+  otherLocked.unlock()
+
+  locked.unlock()
 
   try {
-    docLock(null, ['test', 'other'])()
-    t.pass('did unlock')
+    var finalLock = docLock(null, ['test', 'other'])
+    t.deepEqual(finalLock.failed, [null, null], 'unlock will unlock all ids')
+    finalLock.unlock()
     t.end()
   } catch (err) {
     t.end(err)
@@ -77,29 +108,41 @@ test('docLock should handle multiple docs', function (t) {
 test('docLock should handle prefixes', function (t) {
   t.plan(1)
 
-  var unlock = docLock('user/', 'test')
+  var locked = docLock('user/', 'test')
 
   try {
-    docLock(null, 'test')()
-    docLock('other/', 'test')()
+    docLock(null, 'test').unlock()
+    docLock('other/', 'test').unlock()
   } catch (err) {
     t.fail("shouldn't fail with different prefix")
   }
 
   try {
-    docLock('user/', 'test')()
+    docLock('user/', 'test').unlock()
     t.fail("shouldn't fail with same prefix")
   } catch (err) {
     t.is(err.status, 409, 'did error with same prefix')
   }
 
-  unlock()
+  locked.unlock()
 
   try {
-    docLock('user/', 'test')()
+    docLock('user/', 'test').unlock()
   } catch (err) {
     t.fail("shouldn't fail with same prefix")
   }
 
   t.end()
+})
+
+test('docLock should return the error it was passed as document or id', function (t) {
+  t.plan(2)
+
+  var error = new Error('test')
+  var locked = docLock(null, [error, 'testDocId'])
+
+  t.is(locked.failed[0], error, 'failed contains passed error')
+  t.is(locked.failed[1], null, "correct doc doesn't fail")
+
+  locked.unlock()
 })
